5f093f3c47bdcdb0f427d91c1a5476486af7e766
finy <jos666@qq.com>
2015-06-30 11:38:43 +0800

    change scripts error info

--- openvpn/bin/openvpn.py
+++ openvpn/bin/openvpn.py
@@ -7,8 +7,11 @@ import os, re, sys
 
 dirname = os.path.dirname(__file__)
 if dirname != "/opt/zabbix/scripts":
+	print "------------------INSTALl---------------------------"
+	print "openvpn config file add: management localhost 7505"
+	print "restart openvpn"
 	print "Please put this script in /opt/zabbix/scripts directory"
-	print "mkdir -p /opt/zabbix/scripts;mv %s /opt/zabbix/scripts/" % __file__
+	print "command: mkdir -p /opt/zabbix/scripts;mv %s /opt/zabbix/scripts/" % __file__
 	exit()
 command = '''echo load-stats | python -c "import subprocess;import time;a=subprocess.Popen(['nc','127.0.0.1','7505']);time.sleep(0.01);a.kill()" | grep -v Management'''
 data = {}
